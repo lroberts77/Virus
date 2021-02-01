2fffe7dd42d80002eb4dc46b636abc96d28b1aa2
Liam Roberts <liamroberts77@googlemail.com>
2021-02-01 16:48:49 +0000

    added conponents folder and inside ChatFeed.jsx

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import { ChatEngine } from 'react-chat-engine';
 import './App.css';
+import ChatFeed from './components/ChatFeed';
 
 const App = () => {
     return (
         <ChatEngine
         height="100vh"
-        projectID=""
-        userName=""
-        userSecret=""
+        projectID="61412d7d-7625-4be4-9d03-3f184e5217e9"
+        userName="LiamR"
+        userSecret="liamr"
+        renderChatFeed={(chatAppProps) => <ChatFeed { ... chatAppProps } />}
         />
     );
 };
