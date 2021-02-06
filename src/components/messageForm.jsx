6d4427a7174be17a349b6bf362676753053154a7
Liam Roberts <liamroberts77@googlemail.com>
2021-02-06 20:27:06 +0000

    imported icons for buttons and completed logic for sending messages

--- src/components/messageForm.jsx
+++ src/components/messageForm.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { sendMessage, isTyping } from "react-chat-engine";
-
+import { SendOutlined, PictureOutlined } from "@ant-design/icons"
 
 const MessageForm = (props) => {
     const [value, setvalue] = useState('')
@@ -22,6 +22,10 @@ const MessageForm = (props) => {
         isTyping(props, chatId);
     }
 
+    const handleUpload = (event) => {
+        sendMessage(creds,  chatId, { files: event.target.files, text: ''})
+    }
+
     return (
         <div>
             <form className="message-form" onSubmit={handleSubmit}>
@@ -31,6 +35,15 @@ const MessageForm = (props) => {
                 onChange={handleChange}
                 onSubmit={handleSubmit}
                 />
+                <label htmlFor="upload-button">
+                    <span className="image-button">
+                        <PictureOutlined className="picture-icon" />
+                    </span>
+                </label>
+                <input type="file" multiple={false} id="upload-button" style={{ display: "none"}} onChange={handleUpload} />
+                <button type="submit" className="send-button">
+                    <SendOutlined className="send-icon" />
+                </button>
             </form>
         </div>
     );
