e098c5ec59e946bf11b6fde7214a61fab719f72f
Liam Roberts <liamroberts77@googlemail.com>
2021-02-06 19:35:51 +0000

    added functions for form in messageform.jsx

--- src/components/messageForm.jsx
+++ src/components/messageForm.jsx
@@ -1,11 +1,19 @@
 const MessageForm = () => {
+
+    const handleSubmit = () => {
+
+    }
+
+    const handleChange = () => {
+
+    }
     return (
         <div>
             <form className="message-form" onSubmit={handleSubmit}>
                 <input className="message-input"
                 placeholder="send a amessage"
                 value={value}
-                onChange={handleSubmit}
+                onChange={handleChange}
                 />
             </form>
         </div>
