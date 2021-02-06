71d0221e5b92bbf70a59a052c6ccd7b16d64f6bc
Liam Roberts <liamroberts77@googlemail.com>
2021-02-06 20:06:09 +0000

    added logic to handlesubmit and handlechange in messageform.jsx

--- src/components/messageForm.jsx
+++ src/components/messageForm.jsx
@@ -2,16 +2,26 @@ import { useState } from "react";
 import { sendMessage, isTyping } from "react-chat-engine";
 
 
-const MessageForm = () => {
+const MessageForm = (props) => {
     const [value, setvalue] = useState('')
+    const { chatId, creds } = props
 
-    const handleSubmit = () => {
+    const handleSubmit = (event) => {
+        event.preventDefault()
 
+        const text = value.trim();
+
+        if(text.length > 0) sendMessage(creds, chatId, { text });
+        setvalue('');
     }
 
     const handleChange = (event) => {
 
+        setvalue(event.target.value);
+
+        isTyping(props, chatId);
     }
+
     return (
         <div>
             <form className="message-form" onSubmit={handleSubmit}>
