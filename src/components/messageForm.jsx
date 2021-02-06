27cf4201f815bdd3b466fcdedad6192926fcc1d4
Liam Roberts <liamroberts77@googlemail.com>
2021-02-06 19:10:10 +0000

    created skeleton for message form in messageform.jsx

--- src/components/messageForm.jsx
+++ src/components/messageForm.jsx
@@ -1,7 +1,9 @@
 const MessageForm = () => {
     return (
         <div>
-            MessageForm
+            <form className="message-form" onSubmit={handleSubmit}>
+                
+            </form>
         </div>
     );
 };
