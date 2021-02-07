ac1f4a5d2f63c4c661b858d5d95c3b00c3e3cdf6
Liam Roberts <liamroberts77@googlemail.com>
2021-02-07 17:43:42 +0000

    adding user authorisation to handlesubmit in loginform.jsx

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -5,6 +5,32 @@ const LoginForm = () => {
     const [username, setusername] = useState('');
     const [password, setpassword] = useState('');
 
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+        const authObject = { 'Project-ID': "61412d7d-7625-4be4-9d03-3f184e5217e9", 'User-Name': username, 'User-Secret': password }
+
+        try {
+            //username | password => chatengine => return messages
+            await axios.get('https://api.chatengine.io/chats', { headers: authObject })
+
+            localStorage.setItem('username', username);
+            localStorage.setItem('password', password);
+
+            window.location.reload();
+            
+        } catch (error) {
+            
+        }
+
+
+        //if passes => logged in
+        // else error => try username/password ......
+
+
+
+    }
+
     return (
         <div className="wrapper">
             <div className="form">
