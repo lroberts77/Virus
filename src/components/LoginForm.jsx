fefed0fd5929e48f3cade7749f5e67b2e154c740
Liam Roberts <liamroberts77@googlemail.com>
2021-02-07 17:07:59 +0000

    added return to loginform component

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -2,5 +2,15 @@ import { useState } from 'react';
 import axios from 'axios';
 
 const LoginForm = () => {
+    const [username, setusername] = useState('');
+    const [password, setpassword] = useState('');
+
+    return (
+        <div className="wrapper">
+            <div className="form">
+                
+            </div> 
+        </div>
+    )
     
 }
