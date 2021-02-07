b2101251270c2fda4f468754c3daed2b01c95b18
Liam Roberts <liamroberts77@googlemail.com>
2021-02-07 17:33:53 +0000

    added form to loginform return

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -8,7 +8,16 @@ const LoginForm = () => {
     return (
         <div className="wrapper">
             <div className="form">
-                
+                <h1 className="title">Virus</h1>
+                <form onSubmit={handleSubmit}>
+                    <input type="text" value={username} onChange={(e) => setusername(e.target.value)} className="input" placeholder="Username" required />
+                    <input type="password" value={password} onChange={(e) => setpassword(e.target.value)} className="input" placeholder="Password" required />
+                    <div align="center">
+                        <button type="submit" className="button">
+                            <span>Chat</span>
+                        </button>
+                    </div>
+                </form>
             </div> 
         </div>
     )
